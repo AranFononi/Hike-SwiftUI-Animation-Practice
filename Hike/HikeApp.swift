382dedf6f112768c04f1d25596d8e053e761d52a
Aran Fononi <aaronfononi@gmail.com>
2025-03-21 14:24:52 +0330

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Aran Fononi on 21/3/25.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
