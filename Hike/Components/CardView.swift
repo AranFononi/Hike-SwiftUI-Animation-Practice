c041e39e0a4a0e09bcd582c8503373c006953d88
Aran Fononi <aaronfononi@gmail.com>
2025-03-21 15:14:22 +0330

    Add Card Texts

--- Hike/Components/CardView.swift
+++ Hike/Components/CardView.swift
@@ -9,17 +9,43 @@ import SwiftUI
 
 struct CardView: View {
     var body: some View {
+        // MARK: - Card
         ZStack {
             CustomBackgroundView()
-            ZStack {
+            VStack {
+                // MARK: - Header
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52)).foregroundStyle(LinearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom))
+                        Button {
+                            print("the button was pressed")
+                        } label: {
+                            Text("Button")
+                                
+                        }
+                        
+                    }
+                    Text("Fun and enjoyable outdoor activity for friends and families.")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundStyle(Color.customGrayMedium)
+                }
+                .padding(.horizontal, 30)
                 
-                Circle()
-                    .fill(LinearGradient(colors: [Color.customIndigoMedium, Color.customSalmonLight], startPoint: .topLeading, endPoint: .bottomTrailing))
-                    .frame(width: 256, height: 256)
+                // MARK: - Main Content
                 
-                Image("image-1")
-                    .resizable()
-                    .scaledToFit()
+                ZStack {
+                    Circle()
+                        .fill(LinearGradient(colors: [Color.customIndigoMedium, Color.customSalmonLight], startPoint: .topLeading, endPoint: .bottomTrailing))
+                        .frame(width: 256, height: 256)
+                    
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                // MARK: - Footer
             }
         }
         .frame(width: 320, height: 570)
