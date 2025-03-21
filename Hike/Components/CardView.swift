d902a04f810ef0d7a93fbb70a1aa103f482771fe
Aran Fononi <aaronfononi@gmail.com>
2025-03-21 15:03:57 +0330

    Complete Card Components and Replace It

--- Hike/Components/CardView.swift
+++ Hike/Components/CardView.swift
@@ -10,15 +10,19 @@ import SwiftUI
 struct CardView: View {
     var body: some View {
         ZStack {
-            
-            Circle()
-                .fill(LinearGradient(colors: [Color.customIndigoMedium, Color.customSalmonLight], startPoint: .topLeading, endPoint: .bottomTrailing))
-                .frame(width: 256, height: 256)
-            
-            Image("image-1")
-                .resizable()
-                .scaledToFit()
+            CustomBackgroundView()
+            ZStack {
+                
+                Circle()
+                    .fill(LinearGradient(colors: [Color.customIndigoMedium, Color.customSalmonLight], startPoint: .topLeading, endPoint: .bottomTrailing))
+                    .frame(width: 256, height: 256)
+                
+                Image("image-1")
+                    .resizable()
+                    .scaledToFit()
+            }
         }
+        .frame(width: 320, height: 570)
     }
 }
 
