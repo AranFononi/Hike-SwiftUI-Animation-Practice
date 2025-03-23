5fbc436b22dd086cb6085b27afb12bd57ce5aa8a
Aran Fononi <aaronfononi@gmail.com>
2025-03-23 12:02:32 +0330

    Add Button and GradientStyle and create random functions to generate random images

--- Hike/Components/CardView.swift
+++ Hike/Components/CardView.swift
@@ -8,6 +8,20 @@
 import SwiftUI
 
 struct CardView: View {
+    // MARK: - Properties
+    @State private var imageNumber: Int = 1
+    @State private var randomNumber: Int = 1
+    
+    // MARK: - Functions
+    
+    func randomImage() {
+        repeat {
+            randomNumber = Int.random(in: 1...5)
+        } while randomNumber == imageNumber
+        
+        imageNumber = randomNumber
+    }
+    
     var body: some View {
         // MARK: - Card
         ZStack {
@@ -43,11 +57,23 @@ struct CardView: View {
                         .fill(LinearGradient(colors: [Color.customIndigoMedium, Color.customSalmonLight], startPoint: .topLeading, endPoint: .bottomTrailing))
                         .frame(width: 256, height: 256)
                     
-                    Image("image-1")
+                    Image("image-\(imageNumber)")
                         .resizable()
                         .scaledToFit()
+                        .animation(.default,value: imageNumber)
                 }
                 // MARK: - Footer
+                Button {
+                    print("the button pressed")
+                    randomImage()
+                } label: {
+                    Text("Explore More")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(LinearGradient(colors: [.customGreenLight, .customGreenMedium], startPoint: .top, endPoint: .bottom))
+                        .shadow(color: .black.opacity(0.25), radius: 0.25, x: 1, y: 2)
+                }
+                .buttonStyle(GradientButton())
             }
         }
         .frame(width: 320, height: 570)
